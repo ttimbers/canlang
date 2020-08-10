17f6c209112f3ae0162e6e78d27b82437cbab054
ttimbers <tiffany.timbers@stat.ubc.ca>
2020-08-09 21:48:32 -0700

    fixed bug that ignored nie and nos languages

--- R/can_lang.R
+++ R/can_lang.R
@@ -7,7 +7,7 @@
 #' @format A tibble with 178 rows and 6 variables:
 #' \describe{
 #'   \item{category}{Higher level language category, describing whether the language is an Official Canadian language (e.g., English or French), an Aboriginal language, or a Non-Official and Non-Aboriginal language.}
-#'   \item{language}{Language queried about on the Canadian Census.}
+#'   \item{language}{Language queried about on the Canadian Census. Note: n.i.e. means Not included elsewhere, and n.o.s. means Not otherwise specified}
 #'   \item{mother_tongue}{Total Count of Canadians from the Census who reported the language as their mother tongue (excluding institutional residents - 100% data). Mother tongue is generally defined as the language someone was exposed to since birth.}
 #'   \item{most_at_home}{Total Count of Canadians from the Census who reported the language as spoken most often at home (excluding institutional residents - 100% data).}
 #'   \item{most_at_work}{Total Count of Canadians from the Census who reported the language as used most often at work for the population in private households aged 15 years and over who worked since January 1, 2015 (25% sample data).}
